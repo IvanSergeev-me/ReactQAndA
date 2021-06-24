f5c36eb2b092d2b19e383b1d1b4e0c4daf3cbadc
Ivan_Nikolaevich <inovovanya@gmail.com>
2021-06-24 20:17:44 +0300

    user change v2, before AG

--- src/Components/Profile/Change/Change.jsx
+++ src/Components/Profile/Change/Change.jsx
@@ -5,33 +5,38 @@ import { withRouter } from 'react-router';
 import { compose } from 'redux';
 import {changeUserThunk} from '../../../Redux/Reducers/profile-reducer.js';
 import { Field, reduxForm } from 'redux-form';
-import { Input } from '../../Common/Forms/Input.js';
+import { ChangeInput } from '../../Common/Forms/ChangeInput.js';
 import {requiredField, maxLength} from '../../../Assets/Utils/validators/validator.js';
+import { NavLink } from 'react-router-dom';
 
 const Change = (props) =>{
+    
     let onSubmit = (values) =>{
         let newLogin = values.UserNewLogin;
         let newPassword = values.UserNewPassword;
         let newImage = values.UserNewImage;
+        if(!newLogin) newLogin = "old";
+        if(!newPassword) newPassword = "old";
+        if(!newImage) newImage = "old";
         props.updateUser(newLogin, newPassword, newImage);
     }
     return(
         <div className={s.change_wrapper}>
-            change 
-            <ChangeUserForm onSubmit={onSubmit}/>
+            <ChangeUserForm onSubmit={onSubmit} type={props.typeChange}/>
+            <NavLink className={s.form_send} to={"/Profile/Settings"}>Вернуться</NavLink>
         </div>
     );
 }
 const ChangeForm = (props) =>{
+    let type = props.type;
     return(
         <form onSubmit={props.handleSubmit} className={s.form} >
-            <Field validate={[requiredField,maxLength]} component={Input} name={"UserNewLogin"} placeholder="Измените логин"/>
-            <Field validate={[requiredField,maxLength]} component={Input} name={"UserNewPassword"} placeholder="Измените пароль"/>
-            <Field validate={[requiredField]} component={Input} name={"UserNewImage"} placeholder="Измените ссылку на изображение"/>
+            {type=="login"?<Field validate={[requiredField,maxLength]} component={ChangeInput} name={"UserNewLogin"} placeholder="Измените логин"/>:null}
+            {type=="password"?<Field validate={[requiredField,maxLength]} component={ChangeInput} name={"UserNewPassword"} placeholder="Измените пароль"/>:null}
+            {type=="image"?<Field validate={[requiredField]} component={ChangeInput} name={"UserNewImage"} placeholder="Измените ссылку на изображение"/>:null}
             <div className={s.form_button_container}>
                 <button type="submit" className={s.form_send}>Отправить</button>
-            </div>
-            
+            </div> 
         </form>
     );
 };
@@ -44,6 +49,9 @@ class ChangeClass extends React.Component{
     }
     updateUser = (login, password, image) =>{
         let myId = this.props.appDataReducer.data.id;
+        if (login === "old") login = this.props.appDataReducer.data.login;
+        if (password === "old") password = this.props.appDataReducer.data.password;
+        if (image === "old") image = this.props.appDataReducer.data.image;
         this.props.changeUserThunk( myId, login, password, image);
     }
     render(){
